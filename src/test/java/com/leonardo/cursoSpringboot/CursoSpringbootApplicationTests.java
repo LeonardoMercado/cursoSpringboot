7d1da3b7a6e73d616adaec3671f8ffd1d0247fc3
Leonardo Fabio Mercado Benítez <lfmercadobe@gmail.com>
2022-08-31 00:26:58 -0500

    Se inicia el proyecto

---
+++ src/test/java/com/leonardo/cursoSpringboot/CursoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.cursoSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
