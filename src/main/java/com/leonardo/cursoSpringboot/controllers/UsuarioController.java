d5506cd9a16223304e5314fa4097071ac0b9b03b
Leonardo Fabio Mercado Benítez <lfmercadobe@gmail.com>
2022-09-03 21:31:54 -0500

    Se crea el método de controlador para mostrar un usuario

--- src/main/java/com/leonardo/cursoSpringboot/controllers/UsuarioController.java
+++ src/main/java/com/leonardo/cursoSpringboot/controllers/UsuarioController.java
@@ -1,14 +1,17 @@
 package com.leonardo.cursoSpringboot.controllers;
 
+import com.leonardo.cursoSpringboot.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "Probando el controlador";
+    @RequestMapping(value = "usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario("Leonardo","Mercado","lfmercadobe@unal.edu.co","123456");
+        return usuario;
     }
 
 }
